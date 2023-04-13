c9fb1e7cba01fdce57312fce6f1f64686ec6bacb
nrismawardah <nrismawardah@gmail.com>
2023-04-13 14:45:17 +0700

    Menerima elemen array

--- LinearSearch_145/LinearSearch_145.cpp
+++ LinearSearch_145/LinearSearch_145.cpp
@@ -17,4 +17,14 @@ void input()
 		else
 			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
+
+	// Accept array elements
+	cout << "\n------------------\n";
+	cout << "Enter array element \n";
+	cout << "--------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
 }
